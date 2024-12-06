b3a74d620e62d702daba372ae811b9736e2d777a
Killian <killian_machu@ens.univ-artois.fr>
2024-12-06 16:53:15 +0100

    add more filters

--- src/js/modules/Recipes.js
+++ src/js/modules/Recipes.js
@@ -5,14 +5,20 @@ export default function Recipes(jsonFile) {
         nbRecipes: null,
         nbItems: null,
         difficulties: [],
+        categories: [],
         currentDifficulty: '',
-        popup: null,
-        popupId: null,
+        currentPrepTime: '',
+        currentCategory: '',
+        searchQuery: '',
+        recipeDetail: null,
+        recipeDetailId: null,
+        currentStep: 0,
 
         init(){
             this.fetchDatas(jsonFile)
         },
 
+        // récupérer les données du json
         fetchDatas(jsonFile) {
             fetch(jsonFile)
                 .then(response => response.json())
@@ -24,38 +30,123 @@ export default function Recipes(jsonFile) {
                 });
         },
 
+        // définir les variables nécessaires
         setDatas(datas) {
             this.recipes = datas.recipes
-            this.items = this.recipes
-            this.nbRecipes = this.items.length
-            this.nbItems = this.nbRecipes
-            this.difficulties = this.setDifficulties(this.recipes)
+            this.nbRecipes = this.recipes.length
+            this.getItems()
+            this.difficulties = this.setUniqueValues(this.recipes, 'difficulty')
+            this.categories = this.setUniqueValues(this.recipes, 'category')
         },
 
-        setDifficulties(recipes) {
-            const difficulties = new Set()
+        // définir les filtres (uniques)
+        setUniqueValues(recipes, field) {
+            const uniqueValues = new Set();
             recipes.forEach(recipe => {
-                if (recipe.difficulty) {
-                    difficulties.add(recipe.difficulty)
+                if (recipe[field]) {
+                    uniqueValues.add(recipe[field]);
                 }
             });
             
-            return Array.from(difficulties)
+            return Array.from(uniqueValues);
+        },
+        
+
+        // récupérer les items en fonction des filtres
+        getItems() {
+            this.items = this.recipes.filter(recipe => {
+                return this.filterByDifficulty(recipe) &&
+                this.filterByPrepTime(recipe) &&
+                this.filterByCategory(recipe) &&
+                this.filterBySearchQuery(recipe);
+            });
+            
+            this.nbItems = this.items.length  
+            return this.items
         },
 
-        updateItems() {
+        filterByDifficulty(recipe) {
             if (this.currentDifficulty === '') {
-                this.items = this.recipes
-            } else {
-                this.items = this.recipes.filter((filter) => filter.difficulty == this.currentDifficulty)
+                return true
             }
-            this.nbItems = this.items.length            
+            else {
+                return recipe.difficulty === this.currentDifficulty
+            }
         },
 
-        openPopup(recipeId) {
-            this.popupId = this.popupId
-            this.popup = this.items.find((item) => item.id == recipeId)
-        }
+        filterByPrepTime(recipe) {
+            if (this.currentPrepTime === '') {
+                return true
+            }
+            const prepTime = recipe.preparationTime;
 
+            if (this.currentPrepTime === 'short') {
+                return prepTime < 30;
+            } else if (this.currentPrepTime === 'medium') {
+                return prepTime >= 30 && prepTime <= 60;
+            } else if (this.currentPrepTime === 'long') {
+                return prepTime > 60;
+            }
+            return true;
+        },
+
+        filterByCategory(recipe) {
+            if (this.currentCategory === '') {
+                return true
+            }
+            return recipe.category === this.currentCategory;
+        },
+
+        filterBySearchQuery(recipe) {
+            if (this.searchQuery === '') {
+                return true
+            }
+            return recipe.title.toLowerCase().includes(this.searchQuery.toLowerCase());
+        },
+
+        // vérifier si la popup est ouverte grâce aux données de recipeDetail
+        isRecipeOpen(){
+            return this.recipeDetail !== null
+        },
+
+        // définir les valeurs de recipeDetail et recipeDetailId pour ouvrir la popup
+        openRecipe(recipeId) {
+            this.recipeDetailId = recipeId
+            this.recipeDetail = this.items.find((item) => item.id == this.recipeDetailId)
+        },
+
+        // supprimer les données de recipeDetail pour fermer la popup
+        closeRecipe() {
+            this.recipeDetail = null
+            this.currentStep = 0
+        },
+
+        // augmenter les étapes quand on clique sur suivant
+        decreaseCurrentStep() {
+            if (this.isFirstStep()) {
+                return;
+            }
+
+            this.currentStep--
+        },
+
+        // diminuer les étapes quan on clique sur précédent
+        increaseCurrentStep() {
+            if (this.isLastStep()) {
+                return;
+            }
+            
+            this.currentStep++
+        },
+
+        // vérifier si on est à la première étape
+        isFirstStep() {
+            return this.currentStep === 0
+        },
+
+        // vérifier si on est à la dernière étape
+        isLastStep() {
+            return this.currentStep === (this.recipeDetail.instructions.length -1)
+        },
     }
 }
